0e0fe74dd8230dae2c495793cb1614b9e7c47bd0
nouman0705 <snouman2004@gmail.com>
2026-05-09 15:05:40 +0530

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+let sections = document.querySelectorAll("section")
+let links = document.querySelectorAll("li")
+
+// let height = window.innerHeight
+// console.log(height);
+// let screenHeight = screen.height
+// console.log(screenHeight);
+
+
+
