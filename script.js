b4a8b87334575fc519ddbd3c7b7523f08fafdd6e
nouman0705 <snouman2004@gmail.com>
2026-05-10 22:58:34 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let sections = document.querySelectorAll("section")
 let links = document.querySelectorAll("li")
+let arrow=document.getElementById("arrow")
+
+window.addEventListener("scrollend",()=>{
+    arrow.style.opacity="15%"
+    arrow.style.transition="3s"
+})
+
+window.addEventListener("scroll",()=>{
+    arrow.style.opacity="100%"
+    arrow.style.transition="1s"
+})
 
 // let height = window.innerHeight
 // console.log(height);
